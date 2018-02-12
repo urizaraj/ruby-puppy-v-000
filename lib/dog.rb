0bd739e8c204380f1014fabf44dabbb3a4f09cd0
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 16:58:41 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def initialize(name)
+    self.name = name
